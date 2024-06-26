f5f6c7e1c99f24a1ebbf3ef1e26a87f5701415c0
Max Degterev <max@degterev.me>
2024-06-26 23:09:57 +0200

    rus

--- src/app-terms/index.ts
+++ src/app-terms/index.ts
@@ -2,8 +2,9 @@ import { run } from '../utils';
 import type { Options } from './types';
 import en from './en';
 import de from './de';
+import ru from './ru';
 
-const locales = { en, de };
+const locales = { en, de, ru };
 const schema = { company: 'string', email: 'string', appName: 'string' };
 
 export default (locale: keyof typeof locales, options: Options) => (
